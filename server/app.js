5aaaa160e7891e738478fd7accd634cb4c0216e0
love1 <love1dbsb77@hotmail.com>
2019-01-28 00:50:31 -0800

    Fixed a small typo and changed app.js to original state

--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ mongoose.connect(url, { useNewUrlParser: true }, function (err, res) {
 let port = process.env.PORT || 5000;
 
 /** set up routes {API Endpoints} */
-routes(router)
+// routes(router)
 
 /** set up middlewares */
 app.use(cors())
@@ -35,10 +35,10 @@ app.use(helmet())
 app.use('/api', router)
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'frontend/public')))
+app.use(express.static(path.join(__dirname, 'frontend/build')))
 // Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/frontend/public/index.html'))
+  res.sendFile(path.join(__dirname + '/frontend/build/index.html'))
 })
 
 /** start server */
