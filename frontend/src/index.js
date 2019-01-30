79251f70f7a7d575effa056e2dd7973d4a1779ea
love1 <love1dbsb77@hotmail.com>
2019-01-30 00:24:55 -0800

    Initialized customized store for redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,8 @@ import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import rootReducer from './redux/reducer';
-import { createStore } from 'redux';
+import store from './redux/store';
 import { Provider } from 'react-redux';
-
-const store = createStore(rootReducer);
 
 ReactDOM.render(
     <Provider store={store}>
