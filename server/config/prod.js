ad573ddff89e2eb39527031914b63b5f3360502c
love1 <love1dbsb77@hotmail.com>
2019-02-07 17:00:10 -0800

    Added environment variables that are needed

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,7 +1,7 @@
 module.exports = {
-    DB_URI: process.env.DB_URI,
     SECRET: process.env.SECRET,
+    AWS_BUCKET: process.env.AWS_BUCKET,
     AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
     AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
-    STRIPE_SK: process.env.STRIPE_SK
+    AWS_REGION: process.env.AWS_REGION
 }
